1ff1e7a8eacce3711afb1446f3a19e50639a28bc
Worker <59776615+Worker-guap@users.noreply.github.com>
2022-10-16 02:26:41 +0300

    Update connect_db.php

--- php/connect_db.php
+++ php/connect_db.php
@@ -11,11 +11,11 @@
 	$conn = mysqli_connect('127.0.0.1', 'root', 'root', $db_name);
 
 	mysqli_query($conn, "CREATE TABLE IF NOT EXISTS users(
-    id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
-    user_name VARCHAR(100) NOT NULL,
-	secret_word VARCHAR(32) NOT NULL,
-    login VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    pass VARCHAR(60) NOT NULL,
-	expire_pass INT NOT NULL
-)");
+		id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+		user_name VARCHAR(100) NOT NULL,
+		secret_word VARCHAR(32) NOT NULL,
+		login VARCHAR(100) NOT NULL,
+		email VARCHAR(100) NOT NULL,
+		pass VARCHAR(60) NOT NULL,
+		expire_pass INT NOT NULL
+	)");
